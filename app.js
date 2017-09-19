d0b923c29e90555a6346f9dc74bccd20bbd0fcd2
Ariel R. Pedraza <pedrazaa2@gmail.com>
2017-09-19 15:39:34 -0700

    third time trying to commit

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 'use strict';
-
-var userName = prompt('yup, javascript works. whats your name by the way?');
 var score = 0;
-var answer1 = prompt('Ok ' + userName + ', was I born before 1990?');
-var answer2 = prompt('Do I have a twin?');
-var answer3 = prompt('Do I like playing video games?');
-var answer4 = prompt('Was I born in the PNW?');
-var answer5 = prompt('Did I serve in the Air Force?');
 
+var userName = prompt('Yup, javascript works. What\'s your name by the way?');
+console.log('What\'s your name?: ' + userName);
+
+var answer1 = prompt('Ok ' + userName + ', was I born on or after 1990?');
+console.log('was I born on or after 1990?: ' + answer1);
 if (answer1.toUpperCase() === 'N' || answer1.toUpperCase() === 'NO'){
   score++;
 }
-if (answer1.toUpperCase() === 'Y' || answer1.toUpperCase() === 'YES'){
+
+var answer2 = prompt('Do I have a twin?');
+console.log('Do I have a twin?: ');
+if (answer2.toUpperCase() === 'Y' || answer2.toUpperCase() === 'YES'){
   score++;
 }
-if (answer1.toUpperCase() === 'Y' || answer1.toUpperCase() === 'YES'){
+
+var answer3 = prompt('Do I like playing video games?');
+console.log('Do I like playing video games? ');
+if (answer3.toUpperCase() === 'Y' || answer3.toUpperCase() === 'YES'){
   score++;
 }
-if (answer1.toUpperCase() === 'N' || answer1.toUpperCase() === 'YES'){
+
+var answer4 = prompt('Was I born in the PNW?');
+console.log('Was I born in the PNW? ');
+if (answer4.toUpperCase() === 'N' || answer4.toUpperCase() === 'NO'){
   score++;
 }
-if (answer1.toUpperCase() === 'Y' || answer1.toUpperCase() === 'YES'){
+
+var answer5 = prompt('Did I serve in the Air Force?');
+console.log('Did I serve in the Air Force? ');
+if (answer5.toUpperCase() === 'Y' || answer5.toUpperCase() === 'YES'){
   score++;
 }
 
